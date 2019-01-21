cc4ca0277d5c901c32933aa5aed8629dfbd5d23a
roselinapradjanata <roselinapradjanata@gmail.com>
2019-01-21 15:28:41 +0700

    Add initial stock model

---
+++ app/models/stock.py
@@ -0,0 +1,24 @@
+from app import db
+
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    close = db.Column(db.Float, nullable=False)
+    open = db.Column(db.Float, nullable=False)
+    high = db.Column(db.Float, nullable=False)
+    low = db.Column(db.Float, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def save(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
